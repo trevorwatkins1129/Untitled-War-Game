a8e949a34f555563c4e6d0bc264287b74347f414
TheDuckDev <tawatkins1110@gmail.com>
2024-12-06 17:31:18 -0500

    Now with scaling features!

--- main.lua
+++ main.lua
@@ -1,30 +1,38 @@
 hexagon = require "lib.hexagon"
 
 function love.load()
-    camX = 0
-    camY = 0
+    -- Configure units
     unitRadius = 50
     selectedHexagon = {0, 0}
     selectedHexagonCoords = {0, 0}
 
+    -- Load + setup world map
     world = love.graphics.newImage("img/world.jpg")
     mapSize = {world:getWidth(), world:getHeight()}
 
+    -- Prepare hexagon overlay
     grid = hexagon.grid(mapSize[1]/unitRadius, mapSize[2]/unitRadius, unitRadius, true, false)
     gridCanvas = love.graphics.newCanvas(mapSize[1], mapSize[2])
+    selectedCanvas = love.graphics.newCanvas(mapSize[1], mapSize[2])
+
+    -- -- Init camera
+    scale = 0.5
+    camX = -(mapSize[1])/2 + love.graphics.getWidth()
+    camY = -(mapSize[2])/2 + love.graphics.getHeight()
 end
 
 function love.draw()
     love.graphics.setColor(1, 1, 1)
-    love.graphics.draw(world, camX, camY)
+    love.graphics.draw(world, camX, camY, 0, scale)
     love.graphics.setColor(1, 0, 0)
     hexagon.drawGrid(grid, gridCanvas)
-    love.graphics.draw(gridCanvas, camX, camY)
+    love.graphics.draw(gridCanvas, camX, camY, 0, scale)
     love.graphics.setColor(0, 1, 0)
-    love.graphics.print(selectedHexagon, 10, 10)
     if selectedHexagon[1] ~= 0 then
-        drawBoldHexagon(selectedHexagonCoords[1] + camX, selectedHexagonCoords[2] + camY, unitRadius, true)
+        drawBoldHexagon(selectedHexagonCoords[1], selectedHexagonCoords[2], unitRadius, true)
+        love.graphics.draw(selectedCanvas, camX, camY, 0, scale)
     end
+    love.graphics.print(selectedHexagon, 10, 10)
 end
 
 function love.mousemoved(x, y, dx, dy, touch)
@@ -34,9 +42,13 @@ function love.mousemoved(x, y, dx, dy, touch)
     end
 end
 
+function love.wheelmoved(x, y)
+    scale = math.abs(scale + (y / 3))
+end
+
 function love.mousepressed(x, y, button, isTouch, presses)
     if button == 2 then
-        selectedHexagon[1], selectedHexagon[2] = hexagon.toHexagonCoordinates(x-camX, y-camY, grid)
+        selectedHexagon[1], selectedHexagon[2] = hexagon.toHexagonCoordinates((x-camX) / scale, (y-camY) / scale, grid)
         if selectedHexagon[1] == -1 or selectedHexagon[2] == -1 then
             selectedHexagon = {0, 0}
         end
@@ -45,6 +57,7 @@ function love.mousepressed(x, y, button, isTouch, presses)
 end
 
 function drawHexagon(x, y, hexagonSize, pointyTopped)
+--     love.graphics.setCanvas(gridCanvas)
     local vertices = {}
 
     if pointyTopped then
@@ -62,12 +75,15 @@ function drawHexagon(x, y, hexagonSize, pointyTopped)
             table.insert(vertices, y + hexagonSize * math.sin(i * math.pi / 3))
         end
     end
+    love.graphics.polygon("line", vertices)
+    -- love.graphics.setCanvas()
+end
 
 function drawBoldHexagon(x, y, hexagonSize, pointyTopped)
+    love.graphics.setCanvas(selectedCanvas)
+    love.graphics.clear()
     drawHexagon(x, y, hexagonSize, pointyTopped)
     drawHexagon(x, y, hexagonSize - 1, pointyTopped)
     drawHexagon(x, y, hexagonSize + 1, pointyTopped)
-end
-
-    love.graphics.polygon("line", vertices)
+    love.graphics.setCanvas()
 end
